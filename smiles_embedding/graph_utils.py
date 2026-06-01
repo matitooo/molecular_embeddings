ee30123c5283e5bcacec80b76d1770e1f8fad43e
matimatimati <dagostini@uni-potsdam.de>
2026-06-01 18:21:02 +0200

    fixed shape bug

--- smiles_embedding/graph_utils.py
+++ smiles_embedding/graph_utils.py
@@ -1,8 +1,8 @@
-from rdkit import Chem 
+from rdkit import Chem
 from rdkit.Chem import MolFromSmiles
 import torch
 from torch_geometric.data import Data
-import numpy as np
+
 
 def return_dicts():
     return {
@@ -20,124 +20,123 @@ def return_dicts():
         },
         'chirality_dict': {
             'R': 0,
-            'S': 1
+            'S': 1,
+            'OTHER': 2
         },
-        'stereo_dict' :  {
-        'STEREONONE': 0,
-        'STEREOANY': 1,
-        'STEREOZ': 2,
-        'STEREOE': 3
-}
+        'stereo_dict': {
+            Chem.rdchem.BondStereo.STEREONONE: 0,
+            Chem.rdchem.BondStereo.STEREOANY: 1,
+            Chem.rdchem.BondStereo.STEREOZ: 2,
+            Chem.rdchem.BondStereo.STEREOE: 3
+        }
     }
 
-def read_data():
-  with open ('data/smiles_full.txt','r') as f:
-            smiles_list = [s for s in f.read().splitlines() if s.strip()]
-  return smiles_list
 
-def smiles_to_data(smiles: str,dicts,config):
-    mol = MolFromSmiles(smiles)
-    if mol is None:
-        raise ValueError(f"RDKit could not parse SMILES: {smiles}")
-    node_attr = mole_encoding(mol,dicts,config)
-    edge_index, edge_attr = bond_encoding(mol,dicts)
-    return Data(
-        x=torch.FloatTensor(node_attr),
-        edge_index=torch.LongTensor(edge_index).t().contiguous(),
-        edge_attr=torch.FloatTensor(edge_attr),
-    )
+def read_data(data_path):
+    with open(data_path, 'r') as f:
+        return [s for s in f.read().splitlines() if s.strip()]
 
 
-def encode(value,dict,other = True):
-     if other:
-      out = torch.zeros(size= (len(dict)+1,))
-      out[dict[value] if value in dict.keys() else -1] = 1
-      return out
-     else:
-      out = torch.zeros(size= (len(dict),))
-      out[dict[value]] = 1
-      return out
+def encode(value, mapping):
+    out = torch.zeros(len(mapping) + 1)
+    out[mapping.get(value, len(mapping))] = 1
+    return out
 
-def encode_num(value,list):
-     out = torch.zeros(size=(len(list)+1,))
-     out[value if value in list else -1] = 1
-     return out
 
-def mole_encoding(mol,dicts,config):
-    encoded_atoms = []
-    for atom in mol.GetAtoms():
-        encoded_atoms.append(atom_encoding(atom,dicts,config))
-    return np.array(encoded_atoms)
+def encode_num(value, values):
+    out = torch.zeros(len(values) + 1)
+    out[value if value in values else len(values)] = 1
+    return out
 
-def atom_encoding(atom, dicts,config):
-    vector_collection = {}
-    vector_collection['atom_symbol'] = encode(atom.GetSymbol(), dicts['symbol_dict'])
-    vector_collection['degree'] = encode_num(atom.GetDegree(), list(range(10)))
-    vector_collection['charge'] = torch.tensor([atom.GetFormalCharge()])
 
-    vector_collection['radical_electrons'] = torch.tensor([atom.GetNumRadicalElectrons()])
-    vector_collection['hybridization'] = encode(
-        atom.GetHybridization(),
-        dicts['hybridization_dict']
-    )
-    vector_collection['aromatic'] = torch.tensor([int(atom.GetIsAromatic())])
-    vector_collection['total_h'] = encode_num(atom.GetTotalNumHs(), list(range(5)))
-    chirality = atom.GetProp('_CIPCode') if atom.HasProp('_CIPCode') else 'other'
-    vector_collection['chirality'] = encode(chirality, dicts['chirality_dict'])
-    vector_collection['chirality_center'] = torch.tensor([int(atom.HasProp('_ChiralityPossible'))])
-    tensors = [vector_collection[key] for key in config if key in vector_collection.keys()]
-    return tensors
+def atom_encoding(atom, dicts):
+    vc = {}
+
+    vc['atom_symbol'] = encode(atom.GetSymbol(), dicts['symbol_dict'])
+    vc['degree'] = encode_num(atom.GetDegree(), list(range(10)))
+    vc['charge'] = torch.tensor([atom.GetFormalCharge()], dtype=torch.float)
+    vc['radical_electrons'] = torch.tensor([atom.GetNumRadicalElectrons()], dtype=torch.float)
+
+    vc['hybridization'] = encode(atom.GetHybridization(), dicts['hybridization_dict'])
+    vc['aromatic'] = torch.tensor([int(atom.GetIsAromatic())], dtype=torch.float)
+    vc['total_h'] = encode_num(atom.GetTotalNumHs(), list(range(5)))
+
+    chirality = atom.GetProp('_CIPCode') if atom.HasProp('_CIPCode') else 'OTHER'
+    if chirality not in dicts['chirality_dict']:
+        chirality = 'OTHER'
+
+    vc['chirality'] = encode(chirality, dicts['chirality_dict'])
+    vc['chirality_center'] = torch.tensor([int(atom.HasProp('_ChiralityPossible'))], dtype=torch.float)
+
+    order = [
+        'atom_symbol',
+        'degree',
+        'charge',
+        'radical_electrons',
+        'hybridization',
+        'aromatic',
+        'total_h',
+        'chirality',
+        'chirality_center'
+    ]
+
+    return torch.cat([vc[k] for k in order], dim=0)
+
+
+def mole_encoding(mol, dicts):
+    return torch.stack([atom_encoding(a, dicts) for a in mol.GetAtoms()])
+
 
 def bond_encoding(mol, dicts):
-    edge_attr = []
     edge_index = []
-    n = mol.GetNumAtoms()
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                continue
-            bond = mol.GetBondBetweenAtoms(i, j)
-            if bond is None:
-                continue
-            bt = bond.GetBondType()
-            basic_features = torch.tensor([
-                bt == Chem.rdchem.BondType.SINGLE,
-                bt == Chem.rdchem.BondType.DOUBLE,
-                bt == Chem.rdchem.BondType.TRIPLE,
-                bt == Chem.rdchem.BondType.AROMATIC,
-                bond.GetIsConjugated(),
-                bond.IsInRing(),
-            ], dtype=torch.float)
-            stereo_features = encode(
-                bond.GetStereo(),
-                dicts['stereo_dict']
-            ).float()
-            bond_features = torch.cat([
-                basic_features,
-                stereo_features
-            ])
-            edge_attr.append(bond_features)
-            edge_index.append([i, j])
+    edge_attr = []
+
+    for bond in mol.GetBonds():
+        i = bond.GetBeginAtomIdx()
+        j = bond.GetEndAtomIdx()
+
+        bt = bond.GetBondType()
+
+        basic = torch.tensor([
+            bt == Chem.rdchem.BondType.SINGLE,
+            bt == Chem.rdchem.BondType.DOUBLE,
+            bt == Chem.rdchem.BondType.TRIPLE,
+            bt == Chem.rdchem.BondType.AROMATIC,
+            bond.GetIsConjugated(),
+            bond.IsInRing()
+        ], dtype=torch.float)
+
+        stereo = encode(bond.GetStereo(), dicts['stereo_dict']).float()
+
+        feat = torch.cat([basic, stereo])
+
+        edge_index.append([i, j])
+        edge_index.append([j, i])
+
+        edge_attr.append(feat)
+        edge_attr.append(feat)
 
     if len(edge_attr) == 0:
-        edge_attr = torch.empty(
-            (0, 6 + len(dicts['stereo_dict']) + 1),
-            dtype=torch.float
-        )
-        edge_index = torch.empty(
-            (2, 0),
-            dtype=torch.long
-        )
-    else:
-        edge_attr = torch.stack(edge_attr)
-        edge_index = torch.tensor(
-            edge_index,
-            dtype=torch.long
-        ).t().contiguous()
+        edge_index = torch.empty((2, 0), dtype=torch.long)
+        edge_attr = torch.empty((0, 11), dtype=torch.float)
+        return edge_index, edge_attr
+
+    edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()
+    edge_attr = torch.stack(edge_attr)
 
     return edge_index, edge_attr
-    
 
 
+def smiles_to_data(smiles, dicts):
+    mol = MolFromSmiles(smiles)
+    if mol is None:
+        raise ValueError(smiles)
 
+    x = mole_encoding(mol, dicts)
+    edge_index, edge_attr = bond_encoding(mol, dicts)
 
+    return Data(
+        x=x.float(),
+        edge_index=edge_index,
+        edge_attr=edge_attr.float()
+    )
