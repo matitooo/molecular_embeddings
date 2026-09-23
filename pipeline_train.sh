62499b0039ae6b4324be6848c4058349828bcde2
matimatimati <dagostini@uni-potsdam.de>
2026-09-23 20:52:36 +0200

    added training pipeline

---
+++ pipeline_train.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+LOG_DIR="results/"
+LOG_FILE="$LOG_DIR/pipeline_full.log"
+
+mkdir -p "$LOG_DIR"
+
+exec > >(tee -a "$LOG_FILE") 2>&1
+
+set -x
+
+set -eo pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+bash installing/install.sh
+
+source .venv/bin/activate
+
+model_types=("graph" "3d_infomax" "trimnet")
+
+for model in "${model_types[@]}"; do
+    python main.py \
+        --train \
+        --model "${model}" \
+        --kfold \
+done
